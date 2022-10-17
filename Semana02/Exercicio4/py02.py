590dc38cc12ea9f936224bdac890f377073ab83e
Rafael Lins <rafaellinsnobre@gmail.com>
2022-10-17 19:54:42 -0300

    py1

---
+++ Semana02/Exercicio4/py02.py
@@ -0,0 +1,26 @@
+
+mensagem_composta = '''Hello World
+Estou vivo!'''
+mensagem = "Hello World"
+print(mensagem)
+print(len(mensagem))
+print("Caracter especifico:", mensagem[6])
+print(mensagem[0:5])
+print(mensagem.lower())
+print(mensagem.upper())
+print("Contagem de iterações:", mensagem.count('l'))
+print("Achando posicao de string:", mensagem.find('World'))
+
+nova_messagem = mensagem.replace('World', 'Universe')
+print("Nova mensagem:", nova_messagem)
+
+greeting = "Hello"
+name = "Rafael"
+
+message = greeting + ', ' + name + '. Welcome!'
+print(message)
+
+message = '{}, {}. Welcome!'.format(greeting,name)
+print(message)
+
+print(help(str))
