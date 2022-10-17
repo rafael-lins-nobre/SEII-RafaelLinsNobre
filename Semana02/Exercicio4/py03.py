2c0f1ea213492233b8768d897f7bf64d661fb33f
Rafael Lins <rafaellinsnobre@gmail.com>
2022-10-17 20:45:09 -0300

    py3

---
+++ Semana02/Exercicio4/py03.py
@@ -0,0 +1,37 @@
+# Arithmetic Operators:
+# Addition:       3 + 2
+# Subtraction:    3 - 2
+# Multiplication: 3 * 2
+# Division:       3 / 2
+# Floor Division: 3 // 2
+# Exponent:       3 ** 2
+# Modulus:        3 % 2
+
+
+# Comparisons:
+# Equal:            3 == 2
+# Not Equal:        3 != 2
+# Greater Than:     3 > 2
+# Less Than:        3 < 2
+# Greater or Equal: 3 >= 2
+# Less or Equal:    3 <= 2
+
+# Regras aritméticas normais se aplicam em equações no python3
+# Sinais de +=, -=, *= e /= funcionam em python
+# Sinais de comparação clássicos da linguagem C também funcionam para retornar TRUE ou FALSE
+
+print(3/2)
+print(3//2)
+print(3**2)
+print(3%2)
+
+print(abs(-3))
+print(round(3.25,0))
+
+num1 = '3'
+num2 = '7'
+
+num1 = int(num1)
+num2 = int(num2)
+
+print(num1 + num2)
