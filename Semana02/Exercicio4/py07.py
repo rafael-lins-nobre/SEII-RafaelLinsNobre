7fe37a01fcc17fef05bdc96f29ef1f033de36f79
Rafael Lins <rafaellinsnobre@gmail.com>
2022-10-20 19:04:59 -0300

    py07

---
+++ Semana02/Exercicio4/py07.py
@@ -0,0 +1,16 @@
+num = [1,2,3,4,5]
+
+for n in num:
+    break
+    if n == 4:
+        continue #break 
+    print(n)
+    
+for i in range(5,10):
+    break
+    print(i)
+    
+i=0
+while i <= 10:
+    print(i)
+    i += 1
