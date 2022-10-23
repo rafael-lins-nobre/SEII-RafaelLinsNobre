c575f9867c003865ce23ab024ad75ed9870adac1
Rafael Lins <rafaellinsnobre@gmail.com>
2022-10-23 13:36:57 -0300

    py11

---
+++ Semana03/Exercicio04/numpy11.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Broadcasting
+
+# Broadcasting é um mecanismo que permite o numpy trabalhar com arrays de diferentes
+# formatos ao realizar operacoes aritimeticas
+
+x = np.array([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]])
+v = np.array([1, 0, 1])
+y = x + v  # Add v to each row of x using broadcasting
+print(y) 
