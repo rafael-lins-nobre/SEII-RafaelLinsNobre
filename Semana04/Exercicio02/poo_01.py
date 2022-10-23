4887b6bf58b0ffe87f2e1ffe63e9c31f2f17b4f7
Rafael Lins <rafaellinsnobre@gmail.com>
2022-10-23 18:00:37 -0300

    poo_01

---
+++ Semana04/Exercicio02/poo_01.py
@@ -0,0 +1,33 @@
+
+class Primal:
+    
+    def __init__(self,name, age):
+        self.name = name
+        self.age = age
+        #print("Welcome! {}".format(name))
+    
+    def get_name(self):
+        return self.name
+    
+    def get_age(self):
+        return self.age
+        
+    def set_age(self, age):
+        self.age = age
+        
+    def add_one(self, x):
+        return x +1
+    
+    def hello(self):
+        print('Hello!')
+ 
+# p1.hello()
+# print(p1.add_one(3))
+        
+p1 = Primal('Rafael',22)
+print(p1.name)
+print(p1.get_age())
+p2 = Primal('Arthur',19)
+print(p2.get_name())
+p2.set_age(30)
+print(p2.get_age())
