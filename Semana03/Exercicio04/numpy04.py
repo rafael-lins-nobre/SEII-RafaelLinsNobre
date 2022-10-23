aee1d9de547ae83ee4f04ba66009b2ce0e187f18
Rafael Lins <rafaellinsnobre@gmail.com>
2022-10-23 10:56:30 -0300

    np04

---
+++ Semana03/Exercicio04/numpy04.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+l =[1,2,3]
+a = np.array([1,2,3])
+
+#l.append(4)
+# l = l + [4]
+# print(l)
+# #a = a + np.array([4]) #funciona para soma, mas nao add elemento
+# b = np.append(a,[4])
+# print(a)
+# print(b)
+
+#l = l*2
+#print(l)
+#a = a*2
+#print(a)
+
+b = np.sqrt(a)
+print(b)
+c =np.log(a)
+print(c)
+
+# Operacoes matematicas com array normalmente funcionam no escopo de elementos, enquanto
+# para lists funciona para toda a list.
+
