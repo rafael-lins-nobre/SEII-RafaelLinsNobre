9579bfe6e9e43bca1884faf80a3a672c533d3586
Rafael Lins <rafaellinsnobre@gmail.com>
2022-10-23 14:46:19 -0300

    plot

---
+++ Semana03/Exercicio05/plot_1_2_3.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+# A seção 1 do vídeo foi só uma introdução à biblioteca
+
+# A seção 2 foi só mostrando como importar a biblioteca corretamente. Eu tambem tive que procurar como
+# instalar a biblioteca.
+
+# A seção 3 nao teve codigo nenhum.
