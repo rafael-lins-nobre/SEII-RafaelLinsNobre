be6a9579948a2784cad9a7f452047428594ac734
Rafael Lins <rafaellinsnobre@gmail.com>
2022-10-23 20:44:00 -0300

    POO_03

---
+++ Semana04/Exercicio02/poo_03.py
@@ -0,0 +1,37 @@
+class Pet:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def show(self):
+        print(f"Meu nome é {self.name} e eu tenho {self.age} anos.")
+        
+    def speak(self):
+        print("Nao sei que animal eu sou!")
+        
+class Cat(Pet):
+    def __init__(self, name, age, color):
+        super().__init__(name, age)
+        self.color = color
+    
+    def speak(self):
+        print("Meow!")
+    
+    def show(self):
+        print(f"Meu nome é {self.name} e eu tenho {self.age} anos, minha cor é {self.color}.")
+
+class Dog(Pet):
+    def speak(self):
+        print("Bark!")
+        
+p = Pet("Aiko", 8)
+p.show()
+p.speak()
+
+c = Cat("Frida", 3, "Branca")
+c.show()
+c.speak()
+
+d = Dog("Teo", 10)
+d.show()
+d.speak()
